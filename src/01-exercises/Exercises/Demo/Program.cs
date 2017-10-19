8048bc862ac426fcbefdd424a19fc6c6cde64270
Kristjan Toots <kristjan.toots@gmail.com>
2017-10-19 12:23:30 +0300

    Add first simple exercises

---
+++ src/01-exercises/Exercises/Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hei! Tahan meelde jätta kasutaja sisendit");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input);
+
+            Console.ReadLine();
+            // prindi välja "Sina ütled"
+            // küsi kasutaja sisend ja salvesta nii, et saan seda kasutada
+            // prindi välja "Mina ütlen"...
+
+            // oota kasutaja sisendit, et programm kinni ei läheks
+        }
+    }
+}
