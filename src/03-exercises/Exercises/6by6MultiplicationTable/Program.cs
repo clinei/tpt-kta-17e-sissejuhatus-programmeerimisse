a8d43273f9bc776455c79be46a29c30587ffb07c
Kristjan Toots <kristjan.toots@gmail.com>
2017-11-13 14:19:03 +0200

    Create 6b6 multiplication table project

---
+++ src/03-exercises/Exercises/6by6MultiplicationTable/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _6by6MultiplicationTable
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print out 6by6 multiplication table using two nested for loops
+
+
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.Write(i + "\t");
+
+                for (int j = 1; j <= 6; j++)
+                {
+                    if (i > 0)
+                        Console.Write(i * j + "\t");
+
+                    else
+                        Console.Write(j + "\t");
+                }
+                Console.Write("\n");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
