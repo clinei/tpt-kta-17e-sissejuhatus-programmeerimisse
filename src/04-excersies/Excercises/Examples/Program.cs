d77de529eaa529294ab53ba49abfc03018187acf
Kristjan Toots <kristjan.toots@gmail.com>
2017-11-17 17:24:35 +0200

    Methods

---
+++ src/04-excersies/Excercises/Examples/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = GetUserInput();
+            Console.WriteLine($"Hello {input}!");
+            
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        static int GetUserInput()
+        {
+            bool success = true;
+            int value = 0;
+
+            while (success)
+            {
+                string input = Console.ReadLine();
+                success = !int.TryParse(input, out value);
+            }
+
+            return value;
+        }
+
+        static void PrintGreeting(string a1, int a2)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine($"A1 {a1}");
+            Console.WriteLine($"A2 {a2}");
+        }
+
+        static void DoSomething()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
